00d90a4a76e182d3788f86b394fa7ac4fedf19ab
dMario24 <data.mario24@gmail.com>
2024-07-26 07:56:10 +0900

    0725

--- dags/import_db.py
+++ dags/import_db.py
@@ -27,7 +27,9 @@ with DAG(
 
     task_check = BashOperator(
         task_id="check.done",
-        bash_command="bash {{ var.value.CHECK_SH }} {{ds_nodash}}"
+        bash_command="""
+            bash {{ var.value.CHECK_SH }} ~/data/done/{{ds_nodash}}/_DONE
+            """
     )
 
     task_to_csv = BashOperator(
@@ -37,13 +39,21 @@ with DAG(
 
             U_PATH=~/data/count/{{ds_nodash}}/count.log
             CSV_PATH=~/data/csv/{{ds_nodash}}
-            CSV_FILE=~/data/csv/{{ds_nodash}}
+            CSV_FILE=~/data/csv/{{ds_nodash}}/csv.csv
             
             mkdir -p $CSV_PATH
-            
-            cat $U_PATH | awk '{print "{{ds}}," $2 "," $1}' > ${CSV_FILE}
-
+           
+            cat $U_PATH | awk '{print "^{{ds}}^,^" $2 "^,^" $1 "^"}' > ${CSV_FILE}
             echo $CSV_PATH
+        """
+    )
+
+    task_create_tbl = BashOperator(
+        task_id="create.table",
+        bash_command="""
+            SQL={{ var.value.SQL_PATH }}/create_db_table.sql
+            echo "SQL_PATH=$SQL"
+            MYSQL_PWD='{{ var.value.DB_PASSWD }}' mysql -u root < $SQL
         """
     )
 
@@ -51,6 +61,9 @@ with DAG(
         task_id="to.tmp",
         bash_command="""
             echo "to.tmp"
+            CSV_FILE=~/data/csv/{{ds_nodash}}/csv.csv
+            echo $CSV_FILE
+            bash {{ var.value.SH_HOME }}/csv2mysql.sh $CSV_FILE {{ ds }}
         """
     )
 
@@ -58,13 +71,21 @@ with DAG(
         task_id="to.base",
         bash_command="""
             echo "to.base"
+            bash {{ var.value.SH_HOME }}/tmp2base.sh {{ ds }}
         """
     )
 
     task_done = BashOperator(
         task_id="make.done",
         bash_command="""
-            echo "make.done"
+            figlet "make.done.start"
+            
+            DONE_PATH={{ var.value.IMPORT_DONE_PATH }}/{{ds_nodash}}
+            mkdir -p $DONE_PATH
+            echo "IMPORT_DONE_PATH=$DONE_PATH"
+            touch $DONE_PATH/_DONE
+
+            figlet "make.done.end"
         """
     )
 
@@ -80,7 +101,8 @@ with DAG(
     task_start = EmptyOperator(task_id='start')
 
     task_start >> task_check >> task_to_csv
-    task_to_csv >> task_to_tmp >> task_to_base >> task_done
+    task_to_csv >> task_create_tbl
+    task_create_tbl >> task_to_tmp >> task_to_base >> task_done
     task_done >> task_end
 
     task_check >> task_err >> task_end
